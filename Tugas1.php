699327364934527e37d0ddd7d65891512b2ddfd8
Rahman Amarullah - Windows <45584180+amarullahsiregar@users.noreply.github.com>
2021-11-09 23:48:31 +0700

    Update Tugas-1

---
+++ Tugas1.php
@@ -0,0 +1,25 @@
+<?php
+$bilangan_1 = 20;
+$bilangan_2 = 5;
+
+echo "Bilangan 1 = " . $bilangan_1, "<br>";
+echo "Bilangan 2 = " . $bilangan_2, "<br><br>";
+echo "Berikut merupakan hasil dari setiap operasi <br><br><b>PENJUMLAHAN</b><br>";
+echo "Operator : + <br>";
+echo "Hasil : " . ($bilangan_1 + $bilangan_2) . "<br><br>";
+
+echo "<b>PENGURANGAN</b><br>";
+echo "Operator : - <br>";
+echo "Hasil : " . ($bilangan_1 - $bilangan_2) . "<br><br>";
+
+echo "<b>PERKALIAN</b><br>";
+echo "Operator : * <br>";
+echo "Hasil : " . ($bilangan_1 * $bilangan_2) . "<br><br>";
+
+echo "<b>PEMBAGIAN</b><br>";
+echo "Operator : / <br>";
+echo "Hasil : " . ($bilangan_1 / $bilangan_2) . "<br><br>";
+
+echo "<b>MODULUS</b><br>";
+echo "Operator : % <br>";
+echo "Hasil : " . ($bilangan_1 % $bilangan_2) . "<br><br>";
